dde201732c125b34ae7ed9ba17daf831ff3c5ec0
AlejandroRodriguezP <a.rodriguez.p.1995@gmail.com>
2018-01-15 19:48:24 -0600

    Finish coding beesbeesbees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = "find pollen";
+  this.color = "yellow";
+  this.food = "jelly";
+  this.canFly = true;
+  this.treasureChest =  [];
 };
+ForagerBee.prototype.eat = function(){
 
+};
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+};
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.job = "gamble"
+  this.canFly = false;
+  this.color = "grey";
+  this.food = "jelly";
+  this.treasureChest = [];
 };
+RetiredForagerBee.prototype.eat = function(){
 
+};
+RetiredForagerBee.prototype.forage = function(){
+  return "I am too old, let me play cards instead";
+};
+RetiredForagerBee.prototype.gamble = function(treasure){
+  this.treasureChest.push(treasure);
+};
+//need to push
